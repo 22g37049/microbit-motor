b5379bcea8b014de5265ebf9a33fe3d7fc896c67
22g37049 <145568265+22g37049@users.noreply.github.com>
2023-09-27 11:25:53 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 basic.forever(function () {
-	
+    let data = ""
+    if (data == "f") {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
+    }
 })
